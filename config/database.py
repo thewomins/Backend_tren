f95361aae857000f0a531b6f256057bf316084e0
thewomins <thewomins@gmail.com>
2022-05-19 18:31:26 -0400

    [bug fix] fix attached to a different loop in database

--- config/database.py
+++ config/database.py
@@ -1,10 +1,12 @@
 import motor.motor_asyncio
+import asyncio
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 DATABASE_URI = config.get("DATABASE_URI")
 
 client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_URI)
+client.get_io_loop = asyncio.get_running_loop #get the current loop (cursor?)
 #nombre database
 database = client.tren_app
 #nombre coleccion similar a tablas
