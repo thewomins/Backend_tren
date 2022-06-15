9801ce9c129f90379661baf8d6d7bfe53056fe95
thewomins <thewomins@gmail.com>
2022-06-15 19:13:33 -0400

    fixed middleware

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 #se importan rutas
 from routes.tren import tren
 from routes.admin import admin
 from routes.estaciones import estaciones
 from routes.lineas import linea
+
+from threading import Thread
+from Controllers.adminController import adminController
+
 
 #se instancia fastapi 
 app = FastAPI(title="Api [nombre app]",
@@ -23,10 +28,43 @@ app.include_router(linea)
 async def read_root():
   return {"Hello": "World"}
 
+origins = [
+    "http://localhost",
+    "http://localhost:5500",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "http://127.0.0.1:5500"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+def test_singleton() -> None:
+  singleton = adminController()
+  print(singleton)
+
 if __name__ == "__main__":
   #se corre el servidor de fastapi utilizando uvicorn
   #se utiliza en modo local
   #0.0.0.0 se utiliza para ejecutar en publico y reload en false
+  process1 = Thread(target=test_singleton())
+  process2 = Thread(target=test_singleton())
+  process1.start()
+  process2.start()
+
+  print("normal")
+
+  print(adminController())
+  print(adminController())
+
+
+
   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
 
