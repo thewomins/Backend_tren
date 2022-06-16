4ee9eb8d4a115d4c1998d88ebacbaf5ade3c23b4
Felipe Urrutia <thewomins@gmail.com>
2022-06-16 11:09:58 -0400

    fixed estaciones

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from Controllers.adminController import adminController
 #se instancia fastapi 
 app = FastAPI(title="Api [nombre app]",
   description="this is a REST API using fastapi and mongodb",
-  version="0.0.1"
+  version="0.5.1"
 )
 
 #se incuyen las rutas en la instancia de fastapi
--- models/estaciones.py
+++ models/estaciones.py
@@ -3,4 +3,3 @@ from pydantic import BaseModel
 class Estaciones(BaseModel):
     nombre:str
     ciudad:str
-    kilometro:int
