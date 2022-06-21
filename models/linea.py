474ee5cd4ce0ecded12192bbce05bc19be030cba
Felipe Urrutia <thewomins@gmail.com>
2022-06-20 21:57:09 -0400

    fix model linea

--- models/linea.py
+++ models/linea.py
@@ -1,24 +1,17 @@
-from datetime import date
+from datetime import time
 from pydantic import BaseModel
 
-from models.estaciones import Estaciones
 
 class Horario(BaseModel):
-    salida:date
-    llegada:date
+    Hora_salida: str
+    Hora_llegada: str
     id_tren:str
+
+class Estaciones(BaseModel):
+    nombre:str
+    kilometro:int
 
 class Linea(BaseModel):
     nombre_linea:str
     estaciones:list[Estaciones]
     horarios:list[Horario]
-
-#test caja blanca clase
-m1 = Linea(nombre_linea="nombre",
-    estaciones=[{'nombre':"estacion","ciudad":"cuidad",'kilometro':32}],
-    horarios=[{'salida':"2022-05-23",
-                'llegada':"2022-05-23",
-                'id_tren':"id_tren"}]
-)
-
-print(m1.dict())
