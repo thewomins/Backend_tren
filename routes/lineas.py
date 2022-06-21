ed942de7d947179e710cc52ef518d669f539573c
Felipe Urrutia <thewomins@gmail.com>
2022-06-20 21:58:17 -0400

    fixed instance of linea controller

--- routes/lineas.py
+++ routes/lineas.py
@@ -3,19 +3,24 @@ from Controllers.lineaController import lineaController
 from models.linea import Linea
 
 linea = APIRouter()
+lc = lineaController()
 
 @linea.get("/linea",response_model=list[Linea])
 async def get_linea():
-    return await lineaController.estaciones_list_entity()
+    return await lc.linea_list_entity()
+
+@linea.get("/linea/where-estacion-{estacion}",response_model=list)
+async def get_linea(estacion):
+    return await lc.linea_list_by_estacion(estacion)
 
 @linea.post("/linea/post", response_model=Linea)
 async def post_linea(linea:Linea):
-    return await lineaController.post_linea(linea)
+    return await lc.post_linea(linea)
     
 @linea.put("/linea/put-{nombre_linea}",response_model=Linea)
 async def put_linea(linea:Linea,nombre_linea:str):
-    return await lineaController.put_tren(linea, nombre_linea)
+    return await lc.put_linea(linea, nombre_linea)
 
-@linea.delete("/linea-{id}")
+@linea.delete("/linea/delete-{id}")
 async def delete_linea(id:str):
-    return await lineaController.delete_linea(id)
+    return await lc.delete_linea(id)
