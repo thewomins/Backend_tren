68f03cf443a7d0930ba0eb0d4dbfcede4ce82a33
Felipe Urrutia <thewomins@gmail.com>
2022-06-26 16:58:42 -0400

    add qr generator

--- Controllers/ticketController.py
+++ Controllers/ticketController.py
@@ -1,15 +1,34 @@
+from io import BytesIO
 from models.ticket import Ticket
 from config.singleton import SingletonMeta
 from config.database import get_database
+import uuid
+import qrcode
+import qrcode.image.svg
+from fastapi.responses import StreamingResponse
 
 collection = get_database().tickets
 
 class ticketController(metaclass=SingletonMeta):
     #devuelve 1 entidad con sus atributos
     async def ticket_entity(id) -> dict:
-        cursor = collection.find_one({"id":id})
-        #print(cursor)
+        cursor = await collection.find_one({"id":id})
+        cursor.pop("_id")
         return cursor
+
+    async def ticket_entity_by_id_recorrido_y_asiento(id_recorrido:str,n_asiento:int) -> dict:
+        cursor = await collection.find_one({"id_recorrido":id_recorrido,"numero_asiento":n_asiento})
+        cursor.pop("_id")
+        return cursor
+
+    async def create_qr(id):
+        cursor = await collection.find_one({"id":id})
+        cursor.pop("_id")
+        img = qrcode.make(cursor["id"],image_factory=qrcode.image.svg.SvgPathImage)
+        raw = BytesIO()
+        img.save(raw,"SVG")
+        raw.seek(0)
+        return StreamingResponse(raw, media_type="image/svg")
 
     #devuelve una lista de entidades en este caso Linea
     async def ticket_list_entity() -> list:
@@ -20,6 +39,7 @@ class ticketController(metaclass=SingletonMeta):
         return lineas
 
     async def post_ticket(ticket:Ticket) -> dict:
+        ticket.id=str(uuid.uuid4())
         document = ticket.dict()
         result = collection.insert_one(document)
         return document
