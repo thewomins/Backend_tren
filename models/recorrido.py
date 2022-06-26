71c24b4f4b8544227c35c7513e21ac24b3d5da4e
Felipe Urrutia <thewomins@gmail.com>
2022-06-26 16:59:35 -0400

    add recorrido model and ticket

--- models/recorrido.py
+++ models/recorrido.py
@@ -1,10 +1,10 @@
-from datetime import datetime
 from pydantic import BaseModel
 from models.tren import Tren
 
 class Recorrido(BaseModel):
-    id=datetime
-    id_linea=str
-    partida=datetime.time
-    llegada=datetime.time
-    tren=Tren
+    fecha:str
+    id_linea:str
+    partida:str
+    llegada:str
+    tren:Tren
+    id_recorrido:str
--- models/ticket.py
+++ models/ticket.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
 
 class Ticket(BaseModel):
-    id=str
-    id_recorrido=str
-    estacion_inicio=str
-    estacion_destino=str
-    numero_asiento=int
-    valor=int
+    id:str
+    id_recorrido:str
+    estacion_inicio:str
+    estacion_destino:str
+    numero_asiento:int
+    valor:int
 
 
     
